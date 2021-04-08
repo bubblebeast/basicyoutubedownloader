63c40301d98e2a068c17502cabd160a082a401f8
Batuhan İNANMIŞ <batuhan.inanmis1@gmail.com>
2021-04-08 20:42:05 +0300

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,35 @@
+from pytube import YouTube
+from tkinter import filedialog
+from tkinter import *
+from tkinter import constants
+import tkinter as tk
+
+def selectDocLocation():
+    window.filename =  filedialog.askdirectory(initialdir = "/",title = "Select file")
+
+
+def getYoutubeUrl():
+    youtubeurl = youtubeurl_entry.get()
+    yt = YouTube(youtubeurl)
+    dl = yt.streams.get_highest_resolution()
+    dl.download(window.filename)
+    completedLabel = tk.Label(window, text="Download Completed...", font="bold", pady=5)
+    completedLabel.pack()
+
+# tkinter as tk
+window = tk.Tk()
+window.geometry("400x300")
+window.title("Youtube Downloader")
+#Get Youtube URL / Youtube adresini kullanıcıdan al
+youtubeurl_label = tk.Label(window, text="Youtube URL")
+youtubeurl_entry = tk.Entry(window)
+youtubeurl_label.pack()
+youtubeurl_entry.pack()
+# Select Dir / İndirilecek konumu seç
+SelectDir = tk.Button(window, text="Select Download Location:", command = selectDocLocation)
+SelectDir.pack()
+#Download Button / İndir Butonu
+DownloadButton = tk.Button(window, text= "Download", command = getYoutubeUrl)
+DownloadButton.pack()
+#Main Loop
+window.mainloop()
